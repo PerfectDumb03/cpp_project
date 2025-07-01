476f596a75bb94153b8b1da58a9aaed76ca18229
Luca <injeqted@gmail.com>
2025-07-01 19:49:48 +0200

    Update Shape class with new attributes and methods

--- include/Shape.h
+++ include/Shape.h
@@ -5,17 +5,27 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#include <array>
+#include <random>
+
+
+
 class Shape {
 protected:
     int m_x;
     int m_y;
-    int m_speed;
+    int m_speed = 5;
+    int thickness = -1;
+    std::array<int,3> m_color; // Pointer to RGB color values
 public:
-    Shape(int x, int y, int speed);
+    Shape(int x, int y, std::array<int,3> color);
     virtual ~Shape() = default;
 
     virtual bool checkCollision(const Shape& FaceRect); //checks if the shape collides with Face-Rect
     void move(); //moves shape for the next frame
     virtual bool checkOutOfBounds(int frameHeight); //checks if shape is completely out of bounds
+    void randomizeColor();
+    void setSpeed(int newValue);
+    void setThickness(int newValue);
 };
 #endif //SHAPE_H
--- src/Shape.cpp
+++ src/Shape.cpp
@@ -4,15 +4,37 @@
 
 #include "../include/Shape.h"
 
-Shape::Shape(int x, int y, int speed): m_x(x), m_y(y), m_speed(speed) {
+Shape::Shape(int x, int y, std::array<int,3> color = {0,255,0}): m_x(x), m_y(y), m_color(color={-1,-1,-1}) {
+    if (color[0]==-1) {
+        randomizeColor();
+    }
 }
+
 void Shape::move() {
     m_y += m_speed;
 }
+
 bool Shape::checkOutOfBounds(int frameHeight) {
     return m_y > frameHeight;
 }
 
 bool Shape::checkCollision(const Shape& FaceRect) {
     return true;
+}
+
+void Shape::randomizeColor() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 255);
+    for (int i = 0; i < 3; i++) {
+        m_color[i] = dis(gen);
+    }
+}
+
+void Shape::setSpeed(int newValue) {
+    m_speed = newValue;
+}
+
+void Shape::setThickness(int newValue) {
+    thickness = newValue;
 }
