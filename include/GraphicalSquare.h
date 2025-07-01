c764d3c8bbf92e9558ee1baf7db3a52f09f0704a
Luca <injeqted@gmail.com>
2025-07-01 22:18:31 +0200

    Add GraphicalSquare Class

---
+++ include/GraphicalSquare.h
@@ -0,0 +1,16 @@
+//
+// Created by Luca on 01.07.2025.
+//
+
+#ifndef GRAPHICALSQUARE_H
+#define GRAPHICALSQUARE_H
+
+
+
+class GraphicalSquare {
+
+};
+
+
+
+#endif //GRAPHICALSQUARE_H
---
+++ src/GraphicalSquare.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Luca on 01.07.2025.
+//
+
+#include "../include/GraphicalSquare.h"
