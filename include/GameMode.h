2f19ea49522ca1ccc7e4464f56693ff8ea70dde4
PerfectDumb03 <107958906+PerfectDumb03@users.noreply.github.com>
2025-07-03 15:38:05 +0200

    Add GameMode.h for child classes
    Uncomment gameStartInput in main.cpp

---
+++ include/GameMode.h
@@ -0,0 +1,17 @@
+//
+// Created by Thomas on 03.07.2025.
+//
+
+#ifndef GAME_MODE_H
+#define GAME_MODE_H
+
+#include "Game.h"
+
+class GameMode {
+public:
+    GameMode() = default;
+    virtual ~GameMode() = default;
+    virtual bool isGameOver() = 0;
+};
+
+#endif // GAME_MODE_H
