7ddbc02965495348d0401bbbb221e81615c40bfc
Luca <injeqted@gmail.com>
2025-07-04 18:22:56 +0200

    Add new Classes for CatchBalls GameMode

---
+++ include/CatchBalls.h
@@ -0,0 +1,12 @@
+//
+// Created by Luca on 04.07.2025.
+//
+
+#ifndef CATCHBALLS_H
+#define CATCHBALLS_H
+#include "GameMode.h"
+
+class Catchballs: public GameMode {
+
+};
+#endif //CATCHBALLS_H
---
+++ src/CatchBalls.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Luca on 04.07.2025.
+//
